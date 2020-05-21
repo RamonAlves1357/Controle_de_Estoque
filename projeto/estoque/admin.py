40fc870a7241063bd2108cf7df81efc9c6e1d92b
RamonAlves1357 <ramon.alves@academico.ifpb.edu.br>
2020-05-21 08:31:15 -0300

    TabularInline

--- projeto/estoque/admin.py
+++ projeto/estoque/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .models import Estoque, EstoqueItens
 
 # Register your models here.
+class EstoqueItensInline(admin.TabularInline):
+    model = EstoqueItens
+    extra = 0
+
 @admin.register(Estoque)
 class EstoqueAdmin(admin.ModelAdmin):
-    list_display=('__str__', 'nf', 'funcionario',)
-    search_fields=('nf',)
-    list_filter=('funcionario',)
-    date_hierarchy='created'
+    inlines = (EstoqueItensInline,)
+    list_display = ('__str__', 'nf')
+    search_fields = ('nf',)
+    list_filter = ('funcionario',)
+    date_hierarchy = 'created'
